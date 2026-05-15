8e47358ff4092e757b37300c3e6a483e19df7361
Camila L. Oliveira (Mi) <ola@camilaloliveira.com>
2026-05-15 19:18:11 -0300

    Fix: License and URI

--- plugin.php
+++ plugin.php
@@ -7,6 +7,8 @@
  * Plugin URI: http://wordpress.org/plugins/acessibilidade-integrada/
  * Author URI: https://go.camilaloliveira.com/dev
  * Text Domain: acessibilidade-integrada
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
